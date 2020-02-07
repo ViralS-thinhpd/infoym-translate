76a91e3056ff429cb0145424f04b2b9fa8d50261
ThinhPhan <phanducthinh96@gmail.com>
2020-02-07 17:21:33 +0700

    fix 1.0.4

--- src/app/Http/Controllers/LangController.php
+++ src/app/Http/Controllers/LangController.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\Http\Controllers;
+namespace Thinhpd\Translate\App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
--- src/app/Http/Middleware/Localization.php
+++ src/app/Http/Middleware/Localization.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\Http\Middleware;
+namespace Thinhpd\Translate\App\Http\Middleware;
 
 use Closure;
 use Illuminate\Support\Facades\App;
